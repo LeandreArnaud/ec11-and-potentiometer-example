460017f6263f17996fafaa5749c4f82c5de4d870
Léandre <>
2023-09-10 22:38:36 +0200

    feat: improve without interupt code

--- without_interupt/without_interupt.ino
+++ without_interupt/without_interupt.ino
@@ -1,63 +1,84 @@
 /*
-  example to drive 2 potentiometers and 2 EC11 rotary encoder using the min amount of pins ans interupts
+  example to drive 2 EC11 rotary encoder using the min amount of pins
 */
 
-const byte chan1pin = 14;
-const byte chan2pin = 16;
-const byte rotApin = 2;
-const byte rotBpin = 18;
+const byte chan1pin = 15;
+const byte rot1Apin = 2;
+const byte rot1Bpin = 3;
+const byte rot2Apin = 4;
+const byte rot2Bpin = 5;
+
+const byte input1pin = 10;
+const byte input2pin = 6;
+
 
 void setup() {
   // chanels
   pinMode(chan1pin, OUTPUT);
-  pinMode(chan2pin, OUTPUT);
 
   // EC11 rotary encoder
-  pinMode(rotApin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(rotApin), rotary1function, FALLING);
-  pinMode(rotBpin, INPUT_PULLUP);
+  pinMode(rot1Apin, INPUT_PULLUP);
+  pinMode(rot1Bpin, INPUT_PULLUP);
+  pinMode(rot2Apin, INPUT_PULLUP);
+  pinMode(rot2Bpin, INPUT_PULLUP);
+  pinMode(input1pin, INPUT_PULLUP);
+  pinMode(input2pin, INPUT_PULLUP);
 }
 
-int previousSens1 = 0;
-int previousSens2 = 0;
-bool previousRotA = digitalRead(rotApin);
-int rotValue = 50;
-int threshold = 70;
+bool previousRot1AState = LOW;
+bool previousRot2AState = LOW;
+bool previous1State = LOW;
+bool previous2State = LOW;
+
+int counter1 = 50;
+int counter2 = 50;
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(chan2pin, HIGH);
+  // enable chan1
   digitalWrite(chan1pin, LOW);
-  int sensorVal1 = analogRead(10);
 
-  digitalWrite(chan2pin, LOW);
-  digitalWrite(chan1pin, HIGH);
-  int sensorVal2 = analogRead(10);
-
-  if (previousSens1 - sensorVal1 > threshold || previousSens1 - sensorVal1 < -threshold) {
-    Serial.print("potar1 is ");
-    Serial.println(sensorVal1);
-    previousSens1 = sensorVal1;
+  // but1
+  bool but1State = digitalRead(input1pin);
+  if (!but1State && previous1State) {
+    Serial.println("but1");
   }
-  if (previousSens2 - sensorVal2 > threshold || previousSens2 - sensorVal2 < -threshold) {
-    Serial.print("potar2 is ");
-    Serial.println(sensorVal2);
-    previousSens2 = sensorVal2;
-  }
-}
+  previous1State = but1State;
 
-void rotary1function() {
-  // bool rotA = digitalRead(rotApin);
-  // if (rotA != previousRotA && rotA) {
-  //   if (digitalRead(rotBpin) == rotA) {
-  //     rotValue--;
-  //   } else {
-  //     rotValue++;
-  //   }
-  //   Serial.print("rotary1 is ");
-  //   Serial.println(rotValue);
-  // }
-  // previousRotA = rotA;
-  Serial.println("interupt");
-  delay(1000);
+  // but1
+  bool but2State = digitalRead(input2pin);
+  if (!but2State && previous2State) {
+    Serial.println("but2");
+  }
+  previous2State = but2State;
+
+  // rot1
+  bool rot1AState = digitalRead(rot1Apin);
+  if (!rot1AState && previousRot1AState) {
+    bool rot1BState = digitalRead(rot1Bpin);
+    if (rot1BState) {
+      counter1++;
+    } else {
+      counter1--;
+    }
+    Serial.print("counter1 ");
+    Serial.println(counter1);
+  }
+  previousRot1AState = rot1AState;
+
+  // rot2
+  bool rot2AState = digitalRead(rot2Apin);
+  if (!rot2AState && previousRot2AState) {
+    bool rot2BState = digitalRead(rot2Bpin);
+    if (rot2BState) {
+      counter2++;
+    } else {
+      counter2--;
+    }
+    Serial.print("counter2 ");
+    Serial.println(counter2);
+  }
+  previousRot2AState = rot2AState;
+
+  delay(1);
 }
